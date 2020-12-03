59502735683eda7c27bf31d309e2e0646fe637b3
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 16:12:27 -0700

    Add extra inlaw data

--- inlaws/public/javascripts/app.js
+++ inlaws/public/javascripts/app.js
@@ -3,11 +3,41 @@
 var app = new Vue({
   el: '#app',
   data: {
-    inlaws: [
-      'Inlaw 1',
-      'Inlaw 2',
-      'Inlaw 3',
-      'Inlaw 4'
+    inlaws: [{
+        name: 'Inlaw 1',
+        relation: 'Wife\'s brother',
+        birthday: 'Jan 1, 1990',
+        hobbies: 'Dancing, Trains',
+        notes: 'A little insane.'
+      },
+      {
+        name: 'Inlaw 2',
+        relation: 'Wife\'s sister',
+        birthday: 'Jan 2, 1990',
+        hobbies: 'Dancing, Trains',
+        notes: 'Kinda insane.'
+      },
+      {
+        name: 'Inlaw 3',
+        relation: 'Wife\'s mother',
+        birthday: 'Jan 3, 1990',
+        hobbies: 'Dancing, Trains',
+        notes: 'Insane.'
+      },
+      {
+        name: 'Inlaw 4',
+        relation: 'Wife\'s father',
+        birthday: 'Jan 4, 1990',
+        hobbies: 'Dancing, Trains',
+        notes: 'Pretty insane.'
+      },
+      {
+        name: 'Inlaw 5',
+        relation: 'Wife\'s uncle',
+        birthday: 'Jan 5, 1990',
+        hobbies: 'Dancing, Trains',
+        notes: 'Absolutely insane.'
+      }
     ],
     test: "Hello, World!",
   },
