8a4ed40892fc0fdf77465f78e76537d64e1cc29d
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 19:32:03 -0700

    Add GET for one commment and PUT

--- inlaws/routes/index.js
+++ inlaws/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/inlaws', function(req, res, next) {
+router.get('/api/inlaws', function(req, res, next) {
   Inlaw.find(function(err, inlaws) {
     if (err) {
       return next(err);
@@ -19,7 +19,7 @@ router.get('/inlaws', function(req, res, next) {
   });
 });
 
-router.post('/inlaws', function(req, res, next) {
+router.post('/api/inlaws', function(req, res, next) {
   var inlaw = new Inlaw(req.body);
   inlaw.save(function(err, inlaw) {
     if (err) {
@@ -27,6 +27,42 @@ router.post('/inlaws', function(req, res, next) {
     }
     res.json(inlaw);
   });
+});
+
+router.param('inlaw', function(req, res, next, id) {
+  var query = Inlaw.findById(id);
+  query.exec(function(err, inlaw) {
+    if (err) {
+      return next(err);
+    }
+    if (!inlaw) {
+      return next(new Error("can't find inlaw"));
+    }
+    req.inlaw = inlaw;
+    return next();
+  });
+});
+
+router.get('/api/inlaw/:inlaw', function(req, res) {
+  res.json(req.inlaw);
+});
+
+router.put('/api/inlaws/:id', async (req, res) => {
+  try {
+    let inlaw = await Inlaw.findOne({
+      _id: req.params.id
+    });
+    inlaw.name = req.body.name;
+    inlaw.relation = req.body.relation;
+    inlaw.birthday = req.body.birthday;
+    inlaw.hobbies = req.body.hobbies;
+    inlaw.notes = req.body.notes;
+    await inlaw.save();
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
