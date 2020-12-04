76e03fbf6dae0eea3775959feb5cfddfb004dda1
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 20:48:53 -0700

    Move objects to Home.vue in case I ever figure this out

--- inlaws/src/views/Home.vue
+++ inlaws/src/views/Home.vue
@@ -1,7 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="inlaw in inlaws">
+        <h3>{{inlaw.name}}</h3>
+        <ul>
+          <li>
+            <p> <strong>Relation:</strong> {{inlaw.relation}} </p>
+          </li>
+          <li>
+            <p> <strong>Birthday:</strong> {{inlaw.birthday}} </p>
+          </li>
+          <li>
+            <p> <strong>Hobbies:</strong> {{inlaw.hobbies}} </p>
+          </li>
+          <li>
+            <p> <strong>Notes:</strong> {{inlaw.notes}} </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <form v-on:submit.prevent="addInlaw">
+      <p><strong>Name:</strong></p>
+      <input type="text" v-model="newInlawName">
+      <div class="formSection">
+        <p><strong>Relation:</strong></p>
+        <input type="text" v-model="newInlawRelation">
+        <p><strong>Birthday:</strong></p>
+        <input type="text" v-model="newInlawBirthday">
+        <p><strong>Hobbies:</strong></p>
+        <input type="text" v-model="newInlawHobbies">
+        <p><strong>Notes:</strong></p>
+        <input type="text" v-model="newInlawNotes">
+      </div>
+      <button type="submit">Add</button>
+    </form>
+    <p v-if="warning">You must include a name!</p>
   </div>
 </template>
 
