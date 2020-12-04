7219263922c6503b8e7044780fd0730ba9a61cd6
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 20:56:04 -0700

    Move edit.html to where the router is apparently looking for it

--- inlaws/routes/index.js
+++ inlaws/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/edit', function(req, res, next) {
-  res.render('edit', {
+  res.render('edit.html', {
     title: 'Whatserface - Edit'
   });
 });
