4aedc6a4728527f732f002837d451620a8d1a3c6
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 18:13:23 -0700

    Debugging submission

--- inlaws/routes/index.js
+++ inlaws/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var Inlaw = mongoose.model('Inlaw')
+// var Inlaw = mongoose.model('Inlaw');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
