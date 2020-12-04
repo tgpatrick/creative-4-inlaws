b0ccaeddbf298712c1d87e76100516af2c98266d
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 20:54:27 -0700

    Trying to render edit.html, just in case

--- inlaws/routes/index.js
+++ inlaws/routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/edit', function(req, res, next) {
-  res.render('index', {
+  res.render('edit', {
     title: 'Whatserface - Edit'
   });
 });
