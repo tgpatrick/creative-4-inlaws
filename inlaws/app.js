b7883751417dd3c404fabc3bbfc140e770b1cbd8
Thomas Patrick <thomasgpatrick0203@gmail.com>
2020-12-03 18:54:50 -0700

    Tweak display Add GET/POST API calls

--- inlaws/app.js
+++ inlaws/app.js
@@ -4,12 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/inlaws', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+require('./models/Comments');
+
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
--- inlaws/routes/index.js
+++ inlaws/routes/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-// var Inlaw = mongoose.model('Inlaw');
+var Inlaw = mongoose.model('Inlaw');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Whatserface'
+  });
+});
+
+router.get('/inlaws', function(req, res, next) {
+  Inlaw.find(function(err, inlaws) {
+    if (err) {
+      return next(err);
+    }
+    res.json(inlaws);
+  });
+});
+
+router.post('/inlaws', function(req, res, next) {
+  var inlaw = new Inlaw(req.body);
+  inlaw.save(function(err, inlaw) {
+    if (err) {
+      return next(err);
+    }
+    res.json(inlaw);
   });
 });
 
